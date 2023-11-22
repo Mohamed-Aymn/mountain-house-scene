b6ceb2138c7d679a5f653c5ad3c5689c66f718a1
Mohamed Aymn <mohamedaymn218@gmail.com>
2023-11-22 21:34:34 +0200

    implement project pattern
    functions pattern is created, also some elements are created in this commit such as sun, sky and two trees

--- main.cpp
+++ main.cpp
@@ -1,17 +1,3 @@
-/*
- * GLUT Shapes Demo
- *
- * Written by Nigel Stewart November 2003
- *
- * This program is test harness for the sphere, cone
- * and torus shapes in GLUT.
- *
- * Spinning wireframe and smooth shaded shapes are
- * displayed until the ESC or q key is pressed.  The
- * number of geometry stacks and slices can be adjusted
- * using the + and - keys.
- */
-
 #ifdef __APPLE__
 #include <GLUT/glut.h>
 #else
@@ -21,158 +7,105 @@
 #endif
 
 #include <stdlib.h>
+#include <math.h>
 
-static int slices = 16;
-static int stacks = 16;
-
-/* GLUT callback Handlers */
-
-static void resize(int width, int height)
-{
-    const float ar = (float) width / (float) height;
-
-    glViewport(0, 0, width, height);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    glFrustum(-ar, ar, -1.0, 1.0, 2.0, 100.0);
-
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity() ;
+void sky(){
+    glClear(GL_COLOR_BUFFER_BIT);
 }
 
-static void display(void)
-{
-    const double t = glutGet(GLUT_ELAPSED_TIME) / 1000.0;
-    const double a = t*90.0;
-
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glColor3d(1,0,0);
-
-    glPushMatrix();
-        glTranslated(-2.4,1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutSolidSphere(1,slices,stacks);
-    glPopMatrix();
-
-    glPushMatrix();
-        glTranslated(0,1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutSolidCone(1,1,slices,stacks);
-    glPopMatrix();
-
-    glPushMatrix();
-        glTranslated(2.4,1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutSolidTorus(0.2,0.8,slices,stacks);
-    glPopMatrix();
-
-    glPushMatrix();
-        glTranslated(-2.4,-1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutWireSphere(1,slices,stacks);
-    glPopMatrix();
-
-    glPushMatrix();
-        glTranslated(0,-1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutWireCone(1,1,slices,stacks);
-    glPopMatrix();
-
-    glPushMatrix();
-        glTranslated(2.4,-1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutWireTorus(0.2,0.8,slices,stacks);
-    glPopMatrix();
-
-    glutSwapBuffers();
-}
-
-
-static void key(unsigned char key, int x, int y)
-{
-    switch (key)
-    {
-        case 27 :
-        case 'q':
-            exit(0);
-            break;
-
-        case '+':
-            slices++;
-            stacks++;
-            break;
-
-        case '-':
-            if (slices>3 && stacks>3)
-            {
-                slices--;
-                stacks--;
-            }
-            break;
+void sun(float x, float y, float r, float g, float b){
+    glColor3f(r, g, b);
+    glBegin(GL_POLYGON);
+    // used algorithm in section material :)
+    float angle;
+    for(int i = 0; i < 360; i++){
+        angle = i*3.14/180;
+        glVertex2f(10*cos(angle), 10*sin(angle)+100);
     }
-
-    glutPostRedisplay();
+    glEnd();
 }
 
-static void idle(void)
+
+void Tree(float x, float y, float r, float g, float b) {
+    glColor3f(r, g, b);
+    float scale = 1;
+    float size = 15;
+
+    for(int i = 0; i < 3; i++){
+        glBegin(GL_POLYGON);
+
+        glVertex2i(x+size/scale, y-size/scale);
+        glVertex2i(x, y);
+        glVertex2i(x-size/scale, y-size/scale);
+        glEnd();
+
+        scale+=0.25;
+        y+=9;
+    }
+}
+
+
+void display(void)
 {
-    glutPostRedisplay();
+    /**
+    ** global variables
+    **/
+    float ground = 15; //float [] FLOOR = [100, 15, 100]; // clock wise arrangement
+    float greenTreeColor[3] = {0.04, 0.43, 0.03};
+
+    /**
+    ** elements
+    ** most elements functions have x,y,r,g,b parameters
+    ** (x,y): position
+    ** (r,g,b): colors
+    **/
+    sky();
+    Tree(0, 10, greenTreeColor[0], greenTreeColor[1], greenTreeColor[2]);
+    Tree(100, 10, greenTreeColor[0], greenTreeColor[1], greenTreeColor[2]);
+    sun(0,0, 0.92, 0.89, 0.47);
+
+
+    /** *************** should be called at the end of the display function *************** **/
+    // Performs a buffer swap on the layer in use for the current window. Specifically,
+    // glutSwapBuffers promotes the contents of the back buffer of the layer in use of
+    // the current window to become the contents of the front buffer. The contents of the
+    // back buffer then become undefined. The update typically takes place during the vertical
+    // retrace of the monitor, rather than immediately after glutSwapBuffers is called.
+    glutSwapBuffers();
+    // execute code to the rendering engine
+    glFlush();
 }
 
-const GLfloat light_ambient[]  = { 0.0f, 0.0f, 0.0f, 1.0f };
-const GLfloat light_diffuse[]  = { 1.0f, 1.0f, 1.0f, 1.0f };
-const GLfloat light_specular[] = { 1.0f, 1.0f, 1.0f, 1.0f };
-const GLfloat light_position[] = { 2.0f, 5.0f, 5.0f, 0.0f };
-
-const GLfloat mat_ambient[]    = { 0.7f, 0.7f, 0.7f, 1.0f };
-const GLfloat mat_diffuse[]    = { 0.8f, 0.8f, 0.8f, 1.0f };
-const GLfloat mat_specular[]   = { 1.0f, 1.0f, 1.0f, 1.0f };
-const GLfloat high_shininess[] = { 100.0f };
 
 /* Program entry point */
 
 int main(int argc, char *argv[])
 {
+    // initialize glut library
     glutInit(&argc, argv);
-    glutInitWindowSize(640,480);
-    glutInitWindowPosition(10,10);
-    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
 
-    glutCreateWindow("GLUT Shapes");
+    /*
+        Double buffering involves using two buffers, typically referred to as the
+        front buffer and the back buffer. The front buffer is the one currently being
+        displayed on the screen, while rendering and other operations occur in the back buffer.
+    */
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
 
-    glutReshapeFunc(resize);
+    // window size
+    glutInitWindowSize(1200, 800);
+    // where it should open
+    glutInitWindowPosition(0, 0);
+
+    glutCreateWindow("Giza pyramids");
+
+    // Clear Color buffer, sky color
+    glClearColor(0.81, 0.92, 0.96, 0);
+    // Sets the mode to 2D
+    glMatrixMode(GL_PROJECTION);
+    // Maps the x,y to logical positions
+    gluOrtho2D(0.0, 100.0, 0.0, 100.0);
+
     glutDisplayFunc(display);
-    glutKeyboardFunc(key);
-    glutIdleFunc(idle);
-
-    glClearColor(1,1,1,1);
-    glEnable(GL_CULL_FACE);
-    glCullFace(GL_BACK);
-
-    glEnable(GL_DEPTH_TEST);
-    glDepthFunc(GL_LESS);
-
-    glEnable(GL_LIGHT0);
-    glEnable(GL_NORMALIZE);
-    glEnable(GL_COLOR_MATERIAL);
-    glEnable(GL_LIGHTING);
-
-    glLightfv(GL_LIGHT0, GL_AMBIENT,  light_ambient);
-    glLightfv(GL_LIGHT0, GL_DIFFUSE,  light_diffuse);
-    glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
-    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
-
-    glMaterialfv(GL_FRONT, GL_AMBIENT,   mat_ambient);
-    glMaterialfv(GL_FRONT, GL_DIFFUSE,   mat_diffuse);
-    glMaterialfv(GL_FRONT, GL_SPECULAR,  mat_specular);
-    glMaterialfv(GL_FRONT, GL_SHININESS, high_shininess);
-
     glutMainLoop();
 
     return EXIT_SUCCESS;
