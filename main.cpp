691f47a558ffa1d35573876f4f7f0415517cd25c
Mohamed Aymn <mohamedaymn218@gmail.com>
2023-11-22 22:55:07 +0200

    static clouds

--- main.cpp
+++ main.cpp
@@ -55,6 +55,62 @@ void Tree(float x, float y, float r, float g, float b) {
     }
 }
 
+float cloudX = 0.0f;
+float cloudY = 0.0f;
+float cloudRadius = 5.0f;
+float cloudRotationAngle = 0.0f;
+
+/*
+void cloud(float x, float y, float r, float g, float b){
+    int num_segments = 100;
+    float radius = 5;
+    glBegin(GL_TRIANGLE_FAN);
+    glColor3f(r, g, b);
+    glVertex2f(x, y); // Center of the circle
+
+    for (int j = 0; j < 2; j++){
+        for (int i = 0; i <= num_segments; i++) {
+            float theta = 2.0f * 3.1415926f * float(i) / float(num_segments);
+            float dx = radius * cosf(theta);
+            float dy = radius * sinf(theta);
+            glVertex2f(x + dx, y + dy);
+        }
+        x+=3;
+    }
+
+    glEnd();
+}
+*/
+
+
+
+
+void cloud(float x, float y, float r, float g, float b) {
+    int num_segments = 100;
+    float radius = 5.0f;
+
+    glPushMatrix();
+    glTranslatef(x, y, 0.0f);
+    glRotatef(cloudRotationAngle, 0.0f, 0.0f, 1.0f);
+
+    glBegin(GL_TRIANGLE_FAN);
+    glColor3f(r, g, b);
+    glVertex2f(0.0f, 0.0f); // Center of the circle
+
+    for (int i = 0; i <= num_segments; i++) {
+        float theta = 2.0f * 3.1415926f * float(i) / float(num_segments);
+        float dx = radius * cosf(theta);
+        float dy = radius * sinf(theta);
+        glVertex2f(dx, dy);
+    }
+
+    glEnd();
+
+    glPopMatrix();
+}
+
+
+
 
 void display(void)
 {
@@ -65,6 +121,8 @@ void display(void)
     float groundLevel = 37;
     float greenTreeColor[3] = {0.04, 0.43, 0.03};
 
+
+
     /**
     ** elements
     ** most element functions have these pattern of parameters
@@ -73,6 +131,8 @@ void display(void)
     **/
     sky();
     sun(0,0, 0.92, 0.89, 0.47);
+    cloud(70,90,1,1,1);
+    cloud(30,90,1,1,1);
     ground(groundLevel, 0.72, 0.84, 0.8);
     Tree(0, 10, greenTreeColor[0], greenTreeColor[1], greenTreeColor[2]);
     Tree(100, 10, greenTreeColor[0], greenTreeColor[1], greenTreeColor[2]);
