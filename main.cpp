7310cbd71b32730dbfb637bb60e882ef97632dd5
Nyx <DarkValvet@protonmail.com>
2023-11-24 23:22:01 +0200

    Finlized the project

--- main.cpp
+++ main.cpp
@@ -10,6 +10,11 @@
 #include <math.h>
 #include <iostream>
 
+
+int delta = 0;
+int spokeangle = 0;
+bool left = true;
+
 void sky(){
     glClear(GL_COLOR_BUFFER_BIT);
 }
@@ -37,41 +42,18 @@ void ground(float height, float r, float g, float b){
     glEnd();
 }
 
-
 void mountain(float x, float y, float height, float width, float r, float g, float b){
     // left triangle
     glColor3f(r, g, b);
 
     // mountain structure
     float midpoint = (x+x+width)/2;
-    std::cout << midpoint;
     glBegin(GL_POLYGON);
     glVertex2i(x, y);
     glVertex2i(midpoint, y + height);
     glVertex2i(x+width, y);
     glEnd();
 }
-
-/*
-    glBegin(GL_POLYGON);
-    float angle;
-    for(int i = 0; i < 360; i++){
-        angle = i*3.14/180;
-        glVertex2f(3*cos(angle)+midpoint, 3*sin(angle)+ (y + height-11));
-    }
-    glEnd();
-    glBegin(GL_POLYGON);
-    for(int i = 0; i < 360; i++){
-        angle = i*3.14/180;
-        glVertex2f(3*cos(angle)+(midpoint-6), 3*sin(angle)+ (y + height-11));
-    }
-    glEnd();
-    glBegin(GL_POLYGON);
-    for(int i = 0; i < 360; i++){
-        angle = i*3.14/180;
-        glVertex2f(3*cos(angle)+(midpoint+6), 3*sin(angle)+ (y + height-11));
-    }
-    glEnd();*/
 
 void Tree(float x, float y, float r, float g, float b) {
     glColor3f(r, g, b);
@@ -91,29 +73,10 @@ void Tree(float x, float y, float r, float g, float b) {
     }
 }
 
-/*
-void cloud(float x, float y, float r, float g, float b) {
-    int num_segments = 100;
-    float radius = 5.0f;
-
-    glPushMatrix();
-    glBegin(GL_TRIANGLE_FAN);
-    glColor3f(r, g, b);
-    glVertex2f(0.0f, 0.0f); // Center of the circle
-
-    for (int i = 0; i <= num_segments; i++) {
-        float theta = 2.0f * 3.1415926f * float(i) / float(num_segments);
-        float dx = radius * cosf(theta);
-        float dy = radius * sinf(theta);
-        glVertex2f(dx, dy);
-    }
-
-    glEnd();
-
-    glPopMatrix();
+void Timer(int t){
+    glutTimerFunc(50, Timer, t);
+    glutPostRedisplay();
 }
-*/
-
 
 void cloud(float x, float y, float r, float g, float b){
     int num_segments = 100;
@@ -134,7 +97,6 @@ void cloud(float x, float y, float r, float g, float b){
 
     glEnd();
 }
-
 
 void house(float x, float y){
     // left part
@@ -190,6 +152,101 @@ void house(float x, float y){
     glEnd();
 }
 
+void car(float x, float y, float delta){
+    glColor3f(1, 0, 0);
+    x += delta;
+    //Chassis
+    glBegin(GL_POLYGON);
+    glVertex2i(x, y);
+    glVertex2i(x+20, y);
+    glVertex2i(x+20, y+10);
+    glVertex2i(x, y+10);
+    glEnd();
+
+    // Roof
+    glBegin(GL_POLYGON);
+    glVertex2i(x+4, y+10);
+    glVertex2i(x+16, y+10);
+    glVertex2i(x+16, y+15);
+    glVertex2i(x+4, y+15);
+    glEnd();
+
+    // Window 1
+    glColor3f(0, 0, 0);
+    glBegin(GL_POLYGON);
+    glVertex2i(x+5, y+11);
+    glVertex2i(x+8, y+11);
+    glVertex2i(x+8, y+14);
+    glVertex2i(x+5, y+14);
+    glEnd();
+
+    // Window 2
+    glColor3f(0, 0, 0);
+    glBegin(GL_POLYGON);
+    glVertex2i(x+12, y+11);
+    glVertex2i(x+15, y+11);
+    glVertex2i(x+15, y+14);
+    glVertex2i(x+12, y+14);
+    glEnd();
+
+    // Lights
+    glColor3f(1, 1, 0);
+    if(left){
+        glBegin(GL_POLYGON);
+        glVertex2i(x, y+6);
+        glVertex2i(x+3, y+6);
+        glVertex2i(x+3, y+8);
+        glVertex2i(x, y+8);
+        glEnd();
+    }
+    else{
+        glBegin(GL_POLYGON);
+        glVertex2i(x+20, y+6);
+        glVertex2i(x+17, y+6);
+        glVertex2i(x+17, y+8);
+        glVertex2i(x+20, y+8);
+        glEnd();
+    }
+
+    //Wheels
+    float angle;
+    glColor3f(0,0,0);
+    glBegin(GL_POLYGON);
+    for(int i = 0; i < 360; i++){
+        angle = i*3.14/180;
+        glVertex2f(3*cos(angle)+x+4, 3*sin(angle)+y);
+    }
+    glEnd();
+    glColor3f(0,0,0);
+    glBegin(GL_POLYGON);
+    for(int i = 0; i < 360; i++){
+        angle = i*3.14/180;
+        glVertex2f(3*cos(angle)+x+16, 3*sin(angle)+y);
+    }
+    glEnd();
+
+    //Left Spoke
+    float xr = (x+2+x+6)/2;
+    float yr = (y+y+1)/2;
+    glColor3f(0.5, 0.5, 0.5);
+    glBegin(GL_POLYGON);
+    glVertex2f((xr+((x+2-xr)*cos(spokeangle))-((y-yr)*sin(spokeangle))), (yr+((x+2-xr))*sin(spokeangle))+((y-yr)*cos(spokeangle)));
+    glVertex2f((xr+((x+6-xr)*cos(spokeangle))-((y-yr)*sin(spokeangle))), (yr+((x+6-xr))*sin(spokeangle))+((y-yr)*cos(spokeangle)));
+    glVertex2f((xr+((x+6-xr)*cos(spokeangle))-((y+1-yr)*sin(spokeangle))), (yr+((x+6-xr))*sin(spokeangle))+((y+1-yr)*cos(spokeangle)));
+    glVertex2f((xr+((x+2-xr)*cos(spokeangle))-((y+1-yr)*sin(spokeangle))), (yr+((x+2-xr))*sin(spokeangle))+((y+1-yr)*cos(spokeangle)));
+    glEnd();
+
+    //Right Spoke
+    xr = (x+14+x+18)/2;
+    yr = (y+y+1)/2;
+    glColor3f(0.5, 0.5, 0.5);
+    glBegin(GL_POLYGON);
+    glVertex2f((xr+((x+14-xr)*cos(spokeangle))-((y-yr)*sin(spokeangle))), (yr+((x+14-xr))*sin(spokeangle))+((y-yr)*cos(spokeangle)));
+    glVertex2f((xr+((x+18-xr)*cos(spokeangle))-((y-yr)*sin(spokeangle))), (yr+((x+18-xr))*sin(spokeangle))+((y-yr)*cos(spokeangle)));
+    glVertex2f((xr+((x+18-xr)*cos(spokeangle))-((y+1-yr)*sin(spokeangle))), (yr+((x+18-xr))*sin(spokeangle))+((y+1-yr)*cos(spokeangle)));
+    glVertex2f((xr+((x+14-xr)*cos(spokeangle))-((y+1-yr)*sin(spokeangle))), (yr+((x+14-xr))*sin(spokeangle))+((y+1-yr)*cos(spokeangle)));
+    glEnd();
+}
 
 void display(void)
 {
@@ -199,9 +256,6 @@ void display(void)
     // float ground = 15; //float [] FLOOR = [100, 15, 100]; // clock wise arrangement
     float groundLevel = 37;
     float greenTreeColor[3] = {0.04, 0.43, 0.03};
-
-
-
     /**
     ** elements
     ** most element functions have these pattern of parameters
@@ -222,8 +276,31 @@ void display(void)
     cloud(20, 90, 1, 1, 1);
     ground(groundLevel, 0.72, 0.84, 0.8);
     house(70, 30);
+    car(0,20,delta);
     Tree(0, 10, greenTreeColor[0], greenTreeColor[1], greenTreeColor[2]);
     Tree(100, 10, greenTreeColor[0], greenTreeColor[1], greenTreeColor[2]);
+
+    if(delta == 0){
+        left = false;
+    }
+    else if (delta+20 == 100){
+        left = true;
+    }
+
+    if (spokeangle == 360 || spokeangle == -360){
+        spokeangle = 0;
+    }
+
+    if(left){
+        delta--;
+        spokeangle++;
+    }
+    else{
+        delta++;
+        spokeangle--;
+    }
+
+
 
 
     /** *************** should be called at the end of the display function *************** **/
@@ -255,9 +332,9 @@ int main(int argc, char *argv[])
     // window size
     glutInitWindowSize(1200, 800);
     // where it should open
-    glutInitWindowPosition(0, 0);
+    glutInitWindowPosition(200, 0);
 
-    glutCreateWindow("Mountain scene");
+    glutCreateWindow("[2D Project] Computer Graphics - (CS352) - [211001462, 211003968] [Mohamed Aymn Khanfour, Yehya Mohey]");
 
     // Clear Color buffer, sky color
     glClearColor(0.81, 0.92, 0.96, 0);
@@ -267,6 +344,7 @@ int main(int argc, char *argv[])
     gluOrtho2D(0.0, 100.0, 0.0, 100.0);
 
     glutDisplayFunc(display);
+    Timer(0);
     glutMainLoop();
 
     return EXIT_SUCCESS;
